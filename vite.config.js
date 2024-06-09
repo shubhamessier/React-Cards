2c724978461cccb6540ef26e466fca0133dd3d08
shubhamessier <shubham.gaur7116@gmail.com>
2024-06-09 16:39:50 +0530

    up

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/card/", // Replace 'your-repo-name' with your actual repository name
   plugins: [react()],
-})
+});
